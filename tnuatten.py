c84f24e476922d1f475861cfb5ece74c2d12e371
ana-2511 <130639950+ana-2511@users.noreply.github.com>
2024-05-31 14:16:26 +0530

    tnuatten.py

--- tnuatten.py
+++ tnuatten.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 import datetime
 
-headers = { "authorization": st.secrets{"api_key"}}
+headers = { "authorization": st.secrets["api_key"]}
 # Custom CSS for background image and text color
 st.markdown(
     """
